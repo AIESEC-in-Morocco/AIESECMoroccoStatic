6289ca25f6dae854c7cbf41a32b44584fdb77579
Ziad Abouelfarah <zain.fake02@gmail.com>
2023-04-16 17:09:54 +0000

    lint the pages

--- js/main.js
+++ js/main.js
@@ -1,43 +1,52 @@
-(function($) {
+!(function (t, e, n, c, i, r, a) {
+  (t[n] =
+    t[n] ||
+    function () {
+      (t[n].q = t[n].q || []).push(arguments);
+    }),
+    ((r = e.createElement(c)).async = 1),
+    (r.src = "https://www.clarity.ms/tag/gpiripybkc"),
+    (a = e.getElementsByTagName(c)[0]).parentNode.insertBefore(r, a);
+})(window, document, "clarity", "script");
 
-	"use strict";
+(function ($) {
+  "use strict";
 
-	var fullHeight = function() {
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:true,
-	    dots: true,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-
-	};
-	carousel();
-
+  var carousel = function () {
+    $(".home-slider").owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: true,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-ios-arrow-back'></span>",
+        "<span class='ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+    });
+  };
+  carousel();
 })(jQuery);
